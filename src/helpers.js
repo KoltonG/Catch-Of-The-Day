af31bd7afab25b116b4ea9e26adea6c61fa1bf8f
Kolton Gagnon <Kolton.Gagnon@Gmail.com>
2018-03-27 23:48:15 -0300

    Create helpers.js file
    Create helpers.js file for storing helper functions. These functions will be used throughout the project.

---
+++ src/helpers.js
@@ -0,0 +1,114 @@
+// Helpers File
+
+/**
+ * Price formatter function that takes in cents and outputs USD currency
+ * @param {Number} cents
+ */
+export function formatPrice(cents) {
+  return (cents / 100).toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+  });
+}
+
+/**
+ * Random array item selection function
+ * @param {Object} arr
+ */
+export function rando(arr) {
+  return arr[Math.floor(Math.random * arr.length)];
+}
+
+/**
+ * Turn text into a slug for safe URL's
+ * @param {String} text
+ */
+export function slugify(text) {
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "");
+}
+
+/**
+ * Creates a fun name using random adjectives and nouns
+ */
+export function getFunName() {
+  const adjectives = [
+    "adorable",
+    "beautiful",
+    "clean",
+    "drab",
+    "elegant",
+    "fancy",
+    "glamorous",
+    "handsome",
+    "long",
+    "magnificent",
+    "old-fashioned",
+    "plain",
+    "quaint",
+    "sparkling",
+    "ugliest",
+    "unsightly",
+    "angry",
+    "bewildered",
+    "clumsy",
+    "defeated",
+    "embarrassed",
+    "fierce",
+    "grumpy",
+    "helpless",
+    "itchy",
+    "jealous",
+    "lazy",
+    "mysterious",
+    "nervous",
+    "obnoxious",
+    "panicky",
+    "repulsive",
+    "scary",
+    "thoughtless",
+    "uptight",
+    "worried"
+  ];
+
+  const nouns = [
+    "women",
+    "men",
+    "children",
+    "teeth",
+    "feet",
+    "people",
+    "leaves",
+    "mice",
+    "geese",
+    "halves",
+    "knives",
+    "wives",
+    "lives",
+    "elves",
+    "loaves",
+    "potatoes",
+    "tomatoes",
+    "cacti",
+    "foci",
+    "fungi",
+    "nuclei",
+    "syllabuses",
+    "analyses",
+    "diagnoses",
+    "oases",
+    "theses",
+    "crises",
+    "phenomena",
+    "criteria",
+    "data"
+  ];
+
+  return rando(adjectives) + "-" + rando(adjectives) + "-" + rando(nouns);
+}
